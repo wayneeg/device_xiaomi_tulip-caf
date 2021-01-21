b11d5c33b7b275790a7e797bf3c91451c176d4df
SakilMondal <mondalsakil@gmail.com>
2021-01-21 12:56:16 +0530

    tulip: Drop non-functional soundtrigger configs
    * Fixes google assistant voice match in android 11
    
    Change-Id: I8d20c39f99ba871c327ab74fede4de721f1a4f33

--- device.mk
+++ device.mk
@@ -39,9 +39,7 @@ PRODUCT_SOONG_NAMESPACES += \
 # Audio
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
-    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
-    $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
+    $(DEVICE_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
 
 # Consumerir
 PRODUCT_PACKAGES += \
